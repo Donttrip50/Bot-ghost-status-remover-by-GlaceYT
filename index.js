90e18aeda4cfd0b3c3f01ec6f087554a08db7148
Donttrip50 <jahdarismith27@gmail.com>
2024-10-13 00:59:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["🎧 Listening to s_vi2er1", "🎮 Playing s_vi2er1"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
