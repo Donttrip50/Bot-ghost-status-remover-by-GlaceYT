06155478823acf51589be86e3942c1188c45d6f2
Donttrip50 <jahdarismith27@gmail.com>
2024-11-09 14:34:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to s_vi2er1", "🎮 Playing s_vi2er1"];
+const statusMessages = ["🎧 Listening to 22Slots", "🎮 Playing 22Slots"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
